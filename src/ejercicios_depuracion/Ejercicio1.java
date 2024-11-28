e2e7b9a7f2861a0ce185983b1818de3870a6489c
JSW03280 <jsw03280@educastur.es>
2024-11-28 18:48:21 +0100

    atributos y constructor

--- src/ejercicios_depuracion/Ejercicio1.java
+++ src/ejercicios_depuracion/Ejercicio1.java
@@ -3,6 +3,11 @@ package ejercicios_depuracion;
 import java.util.Scanner;
 
 public class Ejercicio1 {
+    private int suma, num;
+    public Ejercicio1() {
+        this.suma = 0;
+        this.num = 0;
+    }
 
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
